58bfbd2e2f2521a526af17e3f3c770db9bc25a1b
wint-phyusin-maung <sshonn687@gmail.com>
2024-04-25 10:35:23 +0630

    calculate: decimal to binary

---
+++ ch-0/number_system.py
@@ -0,0 +1,20 @@
+decimal_num = 95
+binary_num = bin(decimal_num)
+print("Binary representation of", decimal_num, "is", binary_num)
+
+"""
+#calculation 
+
+95 -> 64 (128 64 36 16 8 4 2 1)
+
+95 - 64 = 31 
+31 - 16 = 15
+15 - 8 = 7
+7 - 4 = 3
+3 - 2 = 1
+
+64 + 16 + 8 + 4 + 2 + 1 = 80 + 15 = 95 
+
+1011111 (binary)
+
+"""
